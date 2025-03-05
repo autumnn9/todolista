87159643d50534b3606dd00aced993cc1772a9a4
autumnn9 <147050980+autumnn9@users.noreply.github.com>
2025-03-05 09:32:22 +0100

    Update lista.sql

--- lista.sql
+++ lista.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 03, 2025 at 08:07 PM
+-- Generation Time: Mar 05, 2025 at 09:32 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `komentarze`
+--
+
+CREATE TABLE `komentarze` (
+  `id` int(11) NOT NULL,
+  `task_id` int(11) NOT NULL,
+  `tresc` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `komentarze`
+--
+
+INSERT INTO `komentarze` (`id`, `task_id`, `tresc`, `created_at`) VALUES
+(2, 98, 'qwerty', '2025-03-05 08:28:59');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `lista`
 --
 
@@ -31,12 +51,29 @@ CREATE TABLE `lista` (
   `id` int(11) NOT NULL,
   `nazwa` varchar(255) NOT NULL,
   `type` varchar(50) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp()
+  `date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `opis` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `lista`
+--
+
+INSERT INTO `lista` (`id`, `nazwa`, `type`, `date`, `opis`) VALUES
+(96, '3', 'praca', '2025-03-05 08:28:39', ''),
+(97, '2', 'dom', '2025-03-05 08:28:41', ''),
+(98, '1', 'inne', '2025-03-05 08:28:44', '');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `komentarze`
+--
+ALTER TABLE `komentarze`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `task_id` (`task_id`);
 
 --
 -- Indexes for table `lista`
@@ -49,10 +86,26 @@ ALTER TABLE `lista`
 --
 
 --
+-- AUTO_INCREMENT for table `komentarze`
+--
+ALTER TABLE `komentarze`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `lista`
 --
 ALTER TABLE `lista`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=99;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `komentarze`
+--
+ALTER TABLE `komentarze`
+  ADD CONSTRAINT `komentarze_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `lista` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
